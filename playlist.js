9e1ecf1c7f56d362f51e5f075fab72436a4533d4
Thales Sollace <maybeviick@gmail.com>
2024-08-25 23:58:29 -0300

    add js

---
+++ playlist.js
@@ -0,0 +1,5 @@
+//criar algo parecido como uma playlist do spotfy no aleatorio
+
+var playlist = [["bad end friends 1", "vmz"], ["bad end friends 2", "vmz"], ["bad end friends 3", "vmz"], ["Punhos de Fogo", "7 minutoz"]];
+
+console.log(playlist, playlist.length, playlist[0], playlist[0].[1])
